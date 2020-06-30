84889c687e9688f7fcc853b03bf062f1527e7d23
Dr1N <drn.exp@gmail.com>
2020-06-30 16:18:58 +0300

    added sting split

---
+++ SplitStrings/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace SplitStrings
+{
+    internal static class Program
+    {
+        static void Main()
+        {
+            Split("abc");
+            Split("abcdef");
+        }
+
+        private static string[] Split(string str)
+        {
+            var list = new List<string>();
+
+            if (!string.IsNullOrEmpty(str))
+            {
+                if (str.Length % 2 != 0)
+                {
+                    str += "_";
+                }
+
+                for (int i = 0; i < str.Length - 1; i += 2)
+                {
+                    list.Add($"{str[i]}{str[i + 1]}");
+                }
+            }
+
+            return list.ToArray();
+        }
+    }
+}
