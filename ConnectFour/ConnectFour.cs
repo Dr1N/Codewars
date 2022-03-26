c6190ea906ae42c3b3a51e969824861a475df6ff
Dr1N <drn.exp@gmail.com>
2022-03-26 02:14:04 +0200

    generate board

--- ConnectFour/ConnectFour.cs
+++ ConnectFour/ConnectFour.cs
@@ -1,9 +1,21 @@
+using System;
 using System.Collections.Generic;
 
 namespace ConnectFour
 {
     public static class ConnectFour
     {
+        private static readonly Dictionary<string, int> ColumnIndexes = new()
+        {
+            {"A", 0 },
+            {"B", 1 },
+            {"C", 2 },
+            {"D", 3 },
+            {"E", 4 },
+            {"F", 5 },
+            {"G", 6 },
+        };
+
         public static string WhoIsWinner(List<string> piecesPositionList)
         {
             var board = GenerateGameBoard(piecesPositionList);
@@ -15,7 +27,16 @@ namespace ConnectFour
 
         private static string[,] GenerateGameBoard(List<string> piecesPositionList)
         {
-            var result = new string[6,7];
+            var result = new string[7, 6];
+            foreach (var element in piecesPositionList)
+            {
+                var parts = element.Split("_");
+                var row = parts[0];
+                var color = parts[1];
+                var rowIndex = ColumnIndexes[row];
+                var colIndex = GetFreeCellInColumn(result, rowIndex);
+                result[rowIndex, colIndex] = color;
+            }
             
             return result;
         }
@@ -31,5 +52,19 @@ namespace ConnectFour
         {
             return string.Empty;
         }
+
+        private static int GetFreeCellInColumn(string[,] board, int row)
+        {
+            for (var y = 5; y >= 0; y--)
+            {
+                var cell = board[row, y];
+                if (string.IsNullOrEmpty(cell))
+                {
+                    return y;
+                }
+            }
+
+            throw new ArgumentException("Invalid column index");
+        } 
     }
 }
